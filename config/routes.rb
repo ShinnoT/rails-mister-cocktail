c0286c6729d3e723b11de5867a27f0081a48cbf3
Shinnosuke Taguchi <shinno.ma30@gmail.com>
2017-10-12 14:24:17 +0900

    routed app correctly for cocktails and doses

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
-
+  resources :cocktails, only: [:index, :show, :new, :create] do
+    resources :doses, only: [:new, :create]
+  end
+  resources :doses, only: [:destroy]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
+
+
+  # resources :restaurants do
+  #   resources :reviews, only: [:new, :create]
+  # end
