cc86d65c32ce5f920a0330d38ff51da7ca0d45ab
Shinnosuke Taguchi <shinno.ma30@gmail.com>
2017-10-12 15:26:01 +0900

    added more instances of ingredients, cocktails, and doses in seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,13 +5,58 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-Ingredient.create(name: "lemon")
-Ingredient.create(name: "ice")
-Ingredient.create(name: "orange")
-Ingredient.create(name: "gin")
-Ingredient.create(name: "rum")
-Ingredient.create(name: "tonic")
-Ingredient.create(name: "vodka")
-Ingredient.create(name: "tequila")
-Ingredient.create(name: "milk")
-Ingredient.create(name: "kahlua")
+
+#reentrant
+#idempotent
+#search for activerecords create_or_update OR find_or_create(this one is useful for user email)
+Ingredient.destroy_all
+Cocktail.destroy_all
+Dose.destroy_all
+
+lemon = Ingredient.create(name: "lemon")
+ice = Ingredient.create(name: "ice")
+orange = Ingredient.create(name: "orange")
+gin = Ingredient.create(name: "gin")
+rum = Ingredient.create(name: "rum")
+tonic = Ingredient.create(name: "tonic")
+vodka = Ingredient.create(name: "vodka")
+tequila = Ingredient.create(name: "tequila")
+milk = Ingredient.create(name: "milk")
+kahlua = Ingredient.create(name: "kahlua")
+
+martini = Cocktail.create(name: "martini")
+bloody_mary = Cocktail.create(name: "bloody mary")
+sour = Cocktail.create(name: "sour")
+mojito = Cocktail.create(name: "mojito")
+daiquiri = Cocktail.create(name: "daiquiri")
+
+
+dose1 = Dose.new(description: "6cl of lemon")
+dose1.ingredient = lemon
+dose1.cocktail = mojito
+dose1.save
+
+dose2 = Dose.new(description: "6 cubes of ice")
+dose2.ingredient = ice
+dose2.cocktail = mojito
+dose2.save
+
+dose3 = Dose.new(description: "7cl of kahlua")
+dose3.ingredient = kahlua
+dose3.cocktail = bloody_mary
+dose3.save
+
+dose4 = Dose.new(description: "20ml of tequila")
+dose4.ingredient = tequila
+dose4.cocktail = bloody_mary
+dose4.save
+
+dose5 = Dose.new(description: "50ml of vodka")
+dose5.ingredient = vodka
+dose5.cocktail = martini
+dose5.save
+
+dose6 = Dose.new(description: "10cl of rum")
+dose6.ingredient = rum
+dose6.cocktail = martini
+dose6.save
